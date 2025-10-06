add7d16be8b9425a4012369ebb4f17b03e2fd7df
waleed ul hassan <wh528947@gmail.com>
2025-10-06 20:16:25 +0500

    Add files via upload

---
+++ arraysearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class SearchElement
+{
+    static void Main()
+    {
+        int[] numbers = new int[10];
+
+        Console.WriteLine("Enter 10 numbers:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Enter number {i + 1}: ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("\nEnter the number you want to search: ");
+        int searchNum = Convert.ToInt32(Console.ReadLine());
+
+        bool found = false;
+
+        // Linear Search
+        for (int i = 0; i < 10; i++)
+        {
+            if (numbers[i] == searchNum)
+            {
+                found = true;
+                break; // stop searching once found
+            }
+        }
+
+        if (found)
+            Console.WriteLine($"\n{searchNum} exists in the array!");
+        else
+            Console.WriteLine($"\n{searchNum} does NOT exist in the array.");
+    }
+}
