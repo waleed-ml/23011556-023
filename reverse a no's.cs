e986214c0fcef0824841e0f27ac585e0394dcd66
waleed ul hassan <wh528947@gmail.com>
2025-10-06 20:10:56 +0500

    Add files via upload

---
+++ reverse a no's.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class ReverseNumber
+{
+    static void Main()
+    {
+        Console.Write("Enter a number to be Reversed : ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int reversed = 0;
+        while (num != 0)
+        {
+            int digit = num % 10;
+            reversed = (reversed * 10) + digit;
+            num = num / 10;
+        }
+        Console.WriteLine("The reversed number is : " + (reversed));
+    }
+
+}
