3b90ea7df34c4ca6e681175eb51511a278bead78
waleed ul hassan <wh528947@gmail.com>
2025-10-06 20:04:01 +0500

    Add files via upload

---
+++ multiplication table.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+class MultiplicationTable
+{
+    static void Main()
+    {
+        Console.Write("Enter the Number of the Table :");
+        int num = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(num + "x" + i + "=" + (num * i));
+        }
+        Console.ReadLine();
+    }
+}
