90bfe8d8cbc7a795dec85fa054a9db5dc02b8b95
waleed ul hassan <wh528947@gmail.com>
2025-10-06 20:14:42 +0500

    Add files via upload

---
+++ checker even or odd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+
+class Checker
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number % 2 == 0)
+            Console.WriteLine("Number is Even");
+        else
+            Console.WriteLine("Number is Odd");
+
+    }
+}
