fef82323794b35038a905fba56447379d8633ac4
waleed ul hassan <wh528947@gmail.com>
2025-10-06 20:01:49 +0500

    Add files via upload

---
+++ sumof natural no's.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+class SumOfNaturalNumbers
+{
+    static void Main()
+    {
+        Console.Write("Enter the number (n) :");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+        for (int i = 0; i <= n; i++)
+        {
+            sum = sum + i;
+        }
+        Console.WriteLine($"The sum of numbers from 1 to {n} is : {sum}");
+    }
+}
