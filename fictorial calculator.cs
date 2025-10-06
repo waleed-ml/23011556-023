c6c926a231c36a5c24f16bb3b0766d8d92cebe87
waleed ul hassan <wh528947@gmail.com>
2025-10-06 20:05:43 +0500

    Add files via upload

---
+++ fictorial calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+
+class Factorial
+{
+    static void Main()
+    {
+        Console.Write("Enter a number to find its factorial : ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int i = 1;
+        int factorial = 1;
+        while (i <= num)
+        {
+            factorial = factorial * i;
+            i++;
+        }
+        Console.WriteLine("The factorial of the entered number is : " + (factorial));
+
+
+    }
