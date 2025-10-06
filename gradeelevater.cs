04d836f5e03bd924d9afb3a6c0ad82793326e446
waleed ul hassan <wh528947@gmail.com>
2025-10-06 19:59:28 +0500

    Add files via upload

---
+++ gradeelevater.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class GardeEvaluator
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the Marks :");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number >= 85)
+        {
+            Console.WriteLine("The Grade is A");
+        }
+        else if (number >= 70 && number <= 84)
+        {
+            Console.WriteLine("The Grade is B");
+        }
+        else if (number >= 55 && number <= 69)
+        {
+            Console.WriteLine("The Grade is C");
+        }
+        else if (number >= 40 && number <= 54)
+        {
+            Console.WriteLine("The Grade is D");
+        }
+        else
+        {
+            Console.WriteLine("The Grade is F");
+        }
+    }
+}
+
